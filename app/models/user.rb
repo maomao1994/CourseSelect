b97f3fa5c22281b69be0f834646ce441658fc100
maomao1994 <tianmao1994@yahoo.com>
2017-12-18 05:57:38 -0800

    activate 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,8 +55,9 @@ class User < ActiveRecord::Base
 
   # Activates an account.
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    # update_attribute(:activated,    true)
+    # update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
   end
 
   # Sends activation email.
