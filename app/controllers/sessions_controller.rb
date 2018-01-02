8a46f7a828b0012c9ffcd084e20b8f3b90cbb228
maomao1994 <tianmao1994@yahoo.com>
2018-01-02 04:25:33 -0800

    activation01

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,8 @@ class SessionsController < ApplicationController
         log_in user
         params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
         flash= {:info => "欢迎回来: #{user.name}，#{user.activated},#{user.email}"}
-
       else
         flash={:danger => 'Account not activated.Check your email for the activation link.'}
-        redirect_to root_url,:flash => flash
       end
     else
       flash= {:danger => '账号或密码错误'}
