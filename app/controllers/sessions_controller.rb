474c016616a3dd25ff103faf0cfe730dd59fc895
maomao1994 <tianmao1994@yahoo.com>
2018-01-02 04:19:33 -0800

    activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,13 @@ class SessionsController < ApplicationController
       #判断用户是否激活？
       if user.activated?
         log_in user
+        params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
+        flash= {:info => "欢迎回来: #{user.name}，#{user.activated},#{user.email}"}
+
       else
-        message  = "您的账户没有被激活， "
-        message += "检查您的邮箱，点击激活链接激活账户."
-        flash[:warning] = message
-        redirect_to root_url
+        flash={:danger => 'Account not activated.Check your email for the activation link.'}
+        redirect_to root_url,:flash => flash
       end
-      log_in user
-      params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
-      flash= {:info => "欢迎回来: #{user.name}，#{user.activated},#{user.email}"}
     else
       flash= {:danger => '账号或密码错误'}
     end
